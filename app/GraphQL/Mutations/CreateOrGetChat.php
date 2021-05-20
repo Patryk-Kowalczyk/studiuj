46fe14080e84bf3d31dfe8d2b3b386852d34a96e
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-05-20 22:40:01 +0200

    Create or get chat bug fix

--- app/GraphQL/Mutations/CreateOrGetChat.php
+++ app/GraphQL/Mutations/CreateOrGetChat.php
@@ -18,16 +18,16 @@ class CreateOrGetChat
         $user = Auth::user();
         $authUserInChats = UserInChat::where('user_id', $user->id)->get();
         $receiver = User::findOrFail($args['id']);
+
         //Search chat same for both users
         foreach ($authUserInChats as $authUserInChat){
             $chatUsers = $authUserInChat->chat->usersInChat;
             foreach ($chatUsers as $chatUser){
-                if ($chatUser->id == $args['id']){
+                if ($chatUser->user_id == (int)$args['id']){
                     return $authUserInChat->chat;
                 }
             }
         }
-
 
         $chat = Chat::create();
         $chat->usersInChat()->create([
