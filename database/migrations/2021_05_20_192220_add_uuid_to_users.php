e6553497ac6b5cbf42760d6f9bf9c8b4bd9471a4
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-05-20 21:48:38 +0200

    Changed user routing to uuid

---
+++ database/migrations/2021_05_20_192220_add_uuid_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->uuid('uuid')->default(DB::raw('(UUID())'))->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
